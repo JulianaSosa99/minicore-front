d59acb4b83e3289901366741c0e162ca4e584d32
Juliana Sosa <juliana.sosa@udla.edu.ec>
2025-06-29 15:35:49 -0500

    cambio en routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,25 @@
-import { NgModule }       from '@angular/core';
-import { RouterModule,
-         Routes }         from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { VentasListComponent } from './pages/ventas-list/ventas-list.component';
 import { ComisionesListComponent } from './pages/comisiones-list/comisiones-list.component';
 
 const routes: Routes = [
-  { path: '',          redirectTo: 'ventas', pathMatch: 'full' },
-  { path: 'ventas',    component: VentasListComponent },
-  { path: 'comisiones',component: ComisionesListComponent },
-  { path: '**',        redirectTo: 'ventas' }
+  // Redirige la ruta vacía al listado de ventas
+  { path: '', redirectTo: 'ventas', pathMatch: 'full' },
+
+  // Tus rutas normales
+  { path: 'ventas', component: VentasListComponent },
+  { path: 'comisiones', component: ComisionesListComponent },
+
+  // Opcional: ruta comodín para “Not Found”
+  { path: '**', redirectTo: 'ventas' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    // esto evita reposicionamiento en scroll al navegar
+    scrollPositionRestoration: 'enabled'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
